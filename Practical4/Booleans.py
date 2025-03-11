ea41a7186ae79bba9420b7be0c748cd7c91193b4
mgf2222 <ggxchfcvvy@163.com>
2025-03-11 11:00:29 +0800

    create Booleans.py

---
+++ Practical4/Booleans.py
@@ -0,0 +1,15 @@
+# Boolean variables
+
+# Initialize X as True and Y as False
+X = True
+Y = False
+
+# Create a new variable W which is 'both X and Y'
+W = X and Y
+
+# Truth table for W
+# X | Y | W
+# T | T | T
+# T | F | F
+# F | T | F
+# F | F | F
