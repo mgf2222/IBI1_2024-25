b11b1ba9eca452caa39f7e380233b2359ded1fa6
mgf2222 <ggxchfcvvy@163.com>
2025-03-11 10:51:46 +0800

    change variables.py

--- Practical4/variables.py
+++ Practical4/variables.py
@@ -1,1 +1,16 @@
-
+a=15
+b=75
+c=a+b
+d=90
+e=5
+f=d+e
+if c > e:
+    print("The c is longer.")
+else:
+    print("The e is longer.")
+#c>e
+if c > f:
+    print("The bus commute is longer.")
+else:
+    print("The car commute is longer.")
+# The car commute is quicker because f (95 minutes) is less than c (90 minutes)
