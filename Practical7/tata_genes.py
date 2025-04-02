cac59ec6820498dc0a0a3996f2cfcdc83f9bac14
mgf2222 <ggxchfcvvy@163.com>
2025-04-02 14:16:12 +0800

    change

--- Practical7/tata_genes.py
+++ Practical7/tata_genes.py
@@ -1,4 +1,4 @@
 import re
-input=open('/IBI1_2024-25/Practical7/Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa','r')
+input=open('Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa','r')
 for line in input:
-	print(line)
+	print(line) 
