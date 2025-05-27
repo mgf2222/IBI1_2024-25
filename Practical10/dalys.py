793c1460ea0cc620a52caf3957f752b5a98c7558
mgf2222 <ggxchfcvvy@163.com>
2025-05-27 17:06:49 +0800

    Add the code about   the third colum

--- Practical10/dalys.py
+++ Practical10/dalys.py
@@ -2,20 +2,29 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
-
+# Set working directory
+os.chdir("D:/IBI/IBImidterm/IBI1_2024-25/Practical10")
 # Load data
 dalys_data = pd.read_csv("dalys-rate-from-all-causes.csv")
 
+print(dalys_data.iloc[0:10, 2]) #showing the third column (the year) for the first 10 rows(inclusive)
+# DALYs data recorded in Afghanistan in the 10th year(1999) is 82624.94
+
+# used a Boolean to show DALYs for all countries in 1990
+data1990 = dalys_data[dalys_data["Year"] == 1990]
+print(data1990.head())
+
 # Filter and analyze UK/France data
+# comput the mean DALYs in the UK and France
 uk = dalys_data.loc[dalys_data["Entity"] == "United Kingdom", ["DALYs","Year"]]
 uk_mean = uk["DALYs"].mean()
 france = dalys_data.loc[dalys_data["Entity"] == "France", ["DALYs","Year"]]
 france_mean = france["DALYs"].mean()
-
+#  UK's DALYs are higher than French
 print(f"UK Average DALYs: {uk_mean:.2f}")
 print(f"France Average DALYs: {france_mean:.2f}")
 
-# Plot UK trends
+# Plot UK DALYS trends over time
 plt.plot(uk.Year, uk.DALYs, 'b+')
 plt.title("UK DALYs Trend Over Time")
 plt.xticks(uk.Year, rotation=45)
@@ -28,7 +37,8 @@ plt.legend()
 plt.tight_layout()
 plt.show()
 
-# Compare China and UK
+# The code answer another question
+# Compare China and UK DALYS from 1990 to 2019
 china = dalys_data.loc[dalys_data["Entity"] == "China", ["DALYs", "Year"]]
 uk = dalys_data.loc[dalys_data["Entity"] == "United Kingdom", ["DALYs", "Year"]]
 
